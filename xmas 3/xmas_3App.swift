e8e802fea41b07f0277371988e5bef0851a15957
CodeTeam <nanotrust3@yahoo.com>
2024-12-22 23:03:51 -0500

    Initial Commit

---
+++ xmas 3/xmas_3App.swift
@@ -0,0 +1,17 @@
+//
+//  xmas_3App.swift
+//  xmas 3
+//
+//  Created by Code Team on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct xmas_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xmas 3Tests/xmas_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  xmas_3Tests.swift
+//  xmas 3Tests
+//
+//  Created by Code Team on 12/22/24.
+//
+
+import Testing
+@testable import xmas_3
+
+struct xmas_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
